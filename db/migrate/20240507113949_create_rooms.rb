95e3a0996266ece6293fb74107955bbcff9c4f15
jusna <164776044+jusn4@users.noreply.github.com>
2024-05-09 02:20:22 +0000

    [Add]show_message

--- db/migrate/20240507113949_create_rooms.rb
+++ db/migrate/20240507113949_create_rooms.rb
@@ -1,6 +1,7 @@
 class CreateRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :rooms do |t|
+      t.integer :room_id
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
